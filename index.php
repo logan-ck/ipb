cee78d56a0cfb7e377be3243fa53c4eb8cda51a6
Logan Butler <logan@terum.us>
2015-06-22 23:50:19 -0400

    Added files
    Added basic html/css/styles updating more soon

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtm11/DTD/xhtml-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+			<meta content="IPB, Indy Predators Baseball"/>
+			<meta charset="utf-8"/>
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+			<meta http-equiv="X-UA-Compatible" content="chrome=1, IE=edge">
+			
+			<meta name="description" content="Indy Predators Baseball | Monsters of the Midwest, driving the spirit for baseball in all IPB players."/>
+			<meta name="keywords" content="Baseball, 18u, Indy, Predators, IPB, Base, Ball, Indiana, Preds, Black, Red, 16u, 17u, 14u, 15u, 13u, 12u, 11u, Monsters, of, the, Midwest"/>
+			<meta property="og:title" content="Indy Predators Baseball" />
+			<meta property="og:site_name" content="Monsters of the Midwest"/>
+			<meta property="og:locale" content="en_US" /> 
+			
+			<!-- Cool JavaScripts -->
+			<script type="text/javascript" src="main.js"/></script>
+
+			<!-- Page fonts: Hind, Sans -->
+			<link href='http://fonts.googleapis.com/css?family=Hind' rel='stylesheet' type='text/css'>
+			<link href="css/main.one.css" rel="stylesheet" type="text/css"/>
+			
+			<!-- Font Awesome 4.3.0 -->
+			<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
+		
+			<title>Indy Predators Baseball | Homepage</title>
+		</head>
+		
+		<body>
+			<div class="container">
+				<div class="nav-container">
+					<!-- Navigation -->
+					<div class="nav">
+						<h1 id="logo">Indy <span class="red">Predators</span> <span class="sub">Monsters of the Midwest</span></h1>
+						<nav id="nav"> 
+							<ul class="navigator">
+								<li>Home</li>
+								<li>Roster</li>
+								<li>Schedule</li>
+								<li>Gallery</li>
+								<li>Sponsors</li>
+								<li>Shop</li>
+							</ul>
+						</nav>
+					</div>
+					<!-- // Navigation -->
+				</div>
+				
+				<!-- Content -->
+				<div class="content">
+					<div class="slider">
+					
+					</div>
+					<div class="sidebar">
+						<h4 class="side-header">sidebar header</h4>
+						<div class="inner-sidebar">
+						
+						</div>
+					</div>
+					<div class="act-pag">
+						<h4 class="main-header">main header</h4>
+						<div class="inner-actpg">
+						
+						</div>
+					</div>
+				</div>
+			</div>
+		</body>
+	</html>
